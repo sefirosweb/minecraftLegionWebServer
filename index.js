a85e1951b8b58f3b465aef0009e2ffaff3f5013e
Javier García <javier.garcia@westwing.es>
2020-11-07 22:01:09 +0100

    standard ffix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require("express")
-const http = require("http")
-const socketIo = require("socket.io")
+const express = require('express')
+const http = require('http')
+const socketIo = require('socket.io')
 
 const port = 4001
 // const index = require("./routes/index_routes") un used
@@ -13,152 +13,145 @@ const io = socketIo(server)
 
 const botsConnected = []
 
+io.on('connection', (socket) => {
+  console.log('New client connected')
 
-io.on("connection", (socket) => {
-    console.log("New client connected")
+  socket.on('disconnect', () => {
+    console.log('Client disconnected')
 
-    socket.on("disconnect", () => {
-        console.log("Client disconnected")
+    const find = botsConnected.find(botConection => botConection.socketId === socket.id)
+    if (find === undefined) { return }
 
-        const find = botsConnected.find(botConection => botConection.socketId === socket.id)
-        if (find === undefined) { return }
+    botsConnected.splice(botsConnected.indexOf(find), 1)
 
-        botsConnected.splice(botsConnected.indexOf(find), 1)
+    io.sockets.emit('botsOnline', botsConnected)
+    sendLogs('Disconnected', find.name, socket.id)
+  })
 
-        io.sockets.emit('botsOnline', botsConnected)
-        sendLogs('Disconnected', find.name, socket.id)
-    })
-
-    // When bot logins
-    socket.on('addFriend', (botName) => {
-        const find = botsConnected.find(botConection => botConection.name === botName)
-        if (find === undefined) {
-            botsConnected.push({
-                socketId: socket.id,
-                name: botName,
-                health: 20,
-                food: 20,
-                combat: false,
-                stateMachinePort: null,
-                inventoryPort: null,
-                viewerPort: null
-            })
-        }
-        io.sockets.emit('botsOnline', botsConnected)
-        sendLogs('Login', botName, socket.id)
-    })
-
-    socket.on('getBotsOnline', () => {
-        socket.emit('botsOnline', botsConnected)
-    })
-
-    socket.on('botStatus', (data) => {
-        const botIndex = botsConnected.findIndex((e) => { return e.socketId === socket.id })
-        if (botIndex >= 0) {
-            const message = { ...data, socketId: socket.id }
-            io.sockets.emit('botStatus', message)
-            botsConnected[botIndex][message.type] = message.value
-        }
-    })
-
-    socket.on('botConnect', (message) => {
-        io.sockets.emit('botConnect', message)
-    })
-
-    // Reciving logs
-    socket.on('logs', (data) => {
-        const find = findBotSocket()
-        if (find) {
-            sendLogs(data, find.name, socket.id)
-        }
-    })
-
-
-    // Receiving chatMessage
-    socket.on('sendAction', (data) => {
-        console.log(data)
-        let index
-
-        switch (data.action) { // Action to specific bot
-            case 'sendMessage':
-                io.to(data.socketId).emit("sendMessage", data.value)
-                break
-            case 'startStateMachine':
-                io.to(data.socketId).emit("startStateMachine", data.value)
-                index = botsConnected.findIndex((e) => { return e.socketId === data.socketId })
-                if (index >= 0) {
-                    botsConnected[index]['stateMachinePort'] = data.value.port
-                    io.sockets.emit('botsOnline', botsConnected)
-                }
-                break
-            case 'startInventory':
-                io.to(data.socketId).emit("startInventory", data.value)
-                index = botsConnected.findIndex((e) => { return e.socketId === data.socketId })
-                if (index >= 0) {
-                    botsConnected[index]['inventoryPort'] = data.value.port
-                    io.sockets.emit('botsOnline', botsConnected)
-                }
-                break
-            case 'startViewer':
-                io.to(data.socketId).emit("startViewer", data.value)
-                index = botsConnected.findIndex((e) => { return e.socketId === data.socketId })
-                if (index >= 0) {
-                    botsConnected[index]['viewerPort'] = data.value.port
-                    io.sockets.emit('botsOnline', botsConnected)
-                }
-                break
-            case 'sendDisconnect':
-                io.to(data.socketId).emit("sendDisconnect", data.value)
-                break
-            case 'sendStay':
-                io.to(data.socketId).emit("sendStay", data.value)
-                break
-            case 'sendFollow':
-                io.to(data.socketId).emit("sendFollow", data.value)
-                break
-            case 'sendEndCommands':
-                io.to(data.socketId).emit("sendEndCommands", data.value)
-                break
-
-            case 'sendStartWay':
-                io.to(data.socketId).emit("sendStartWay", data.value)
-                break
-            case 'sendSavePatrol':
-                io.to(data.socketId).emit("sendSavePatrol", data.value)
-                break
-            case 'sendSaveChest':
-                io.to(data.socketId).emit("sendSaveChest", data.value)
-                break
-        }
-
-
-    })
-
-    function findBotSocket() {
-        const bot = botsConnected.find(botConection => botConection.socketId === socket.id)
-        if (bot === undefined) {
-            return false
-        } else {
-            return bot
-        }
+  // When bot logins
+  socket.on('addFriend', (botName) => {
+    const find = botsConnected.find(botConection => botConection.name === botName)
+    if (find === undefined) {
+      botsConnected.push({
+        socketId: socket.id,
+        name: botName,
+        health: 20,
+        food: 20,
+        combat: false,
+        stateMachinePort: null,
+        inventoryPort: null,
+        viewerPort: null
+      })
     }
+    io.sockets.emit('botsOnline', botsConnected)
+    sendLogs('Login', botName, socket.id)
+  })
+
+  socket.on('getBotsOnline', () => {
+    socket.emit('botsOnline', botsConnected)
+  })
+
+  socket.on('botStatus', (data) => {
+    const botIndex = botsConnected.findIndex((e) => { return e.socketId === socket.id })
+    if (botIndex >= 0) {
+      const message = { ...data, socketId: socket.id }
+      io.sockets.emit('botStatus', message)
+      botsConnected[botIndex][message.type] = message.value
+    }
+  })
+
+  socket.on('botConnect', (message) => {
+    io.sockets.emit('botConnect', message)
+  })
+
+  // Reciving logs
+  socket.on('logs', (data) => {
+    const find = findBotSocket()
+    if (find) {
+      sendLogs(data, find.name, socket.id)
+    }
+  })
+
+  // Receiving chatMessage
+  socket.on('sendAction', (data) => {
+    console.log(data)
+    let index
+
+    switch (data.action) { // Action to specific bot
+      case 'sendMessage':
+        io.to(data.socketId).emit('sendMessage', data.value)
+        break
+      case 'startStateMachine':
+        io.to(data.socketId).emit('startStateMachine', data.value)
+        index = botsConnected.findIndex((e) => { return e.socketId === data.socketId })
+        if (index >= 0) {
+          botsConnected[index].stateMachinePort = data.value.port
+          io.sockets.emit('botsOnline', botsConnected)
+        }
+        break
+      case 'startInventory':
+        io.to(data.socketId).emit('startInventory', data.value)
+        index = botsConnected.findIndex((e) => { return e.socketId === data.socketId })
+        if (index >= 0) {
+          botsConnected[index].inventoryPort = data.value.port
+          io.sockets.emit('botsOnline', botsConnected)
+        }
+        break
+      case 'startViewer':
+        io.to(data.socketId).emit('startViewer', data.value)
+        index = botsConnected.findIndex((e) => { return e.socketId === data.socketId })
+        if (index >= 0) {
+          botsConnected[index].viewerPort = data.value.port
+          io.sockets.emit('botsOnline', botsConnected)
+        }
+        break
+      case 'sendDisconnect':
+        io.to(data.socketId).emit('sendDisconnect', data.value)
+        break
+      case 'sendStay':
+        io.to(data.socketId).emit('sendStay', data.value)
+        break
+      case 'sendFollow':
+        io.to(data.socketId).emit('sendFollow', data.value)
+        break
+      case 'sendEndCommands':
+        io.to(data.socketId).emit('sendEndCommands', data.value)
+        break
+
+      case 'sendStartWay':
+        io.to(data.socketId).emit('sendStartWay', data.value)
+        break
+      case 'sendSavePatrol':
+        io.to(data.socketId).emit('sendSavePatrol', data.value)
+        break
+      case 'sendSaveChest':
+        io.to(data.socketId).emit('sendSaveChest', data.value)
+        break
+    }
+  })
+
+  function findBotSocket () {
+    const bot = botsConnected.find(botConection => botConection.socketId === socket.id)
+    if (bot === undefined) {
+      return false
+    } else {
+      return bot
+    }
+  }
 })
 
+function sendLogs (data, botName = '', socketId = '') {
+  const date = new Date()
+  const time = ('0' + date.getHours()).slice(-2) + ':' + ('0' + (date.getMinutes() + 1)).slice(-2) + ':' + ('0' + (date.getSeconds() + 1)).slice(-2)
 
+  const message = {
+    message: data,
+    time,
+    socketId,
+    botName
+  }
 
-function sendLogs(data, botName = '', socketId = '') {
-    const date = new Date()
-    const time = ('0' + date.getHours()).slice(-2) + ':' + ('0' + (date.getMinutes() + 1)).slice(-2) + ':' + ('0' + (date.getSeconds() + 1)).slice(-2)
-
-    const message = {
-        message: data,
-        time,
-        socketId,
-        botName
-    }
-
-    io.sockets.emit('logs', message)
+  io.sockets.emit('logs', message)
 }
 
 server.listen(port, () => console.log(`Listening on port ${port}`))
-
--- routes/index_routes.js
+++ routes/index_routes.js
@@ -1,7 +1,6 @@
 // const express = require("express");
 // const router = express.Router();
 
-
 // router.get("/", (req, res) => {
 //     res.send({ response: "I am alive" }).status(200);
 // });
