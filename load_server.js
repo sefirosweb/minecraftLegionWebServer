18d2077cebfdcc5bb9801749482ec2bdde8509b4
sefirosweb <sefirosweb@gmail.com>
2021-04-11 18:45:09 +0200

    Added events

--- load_server.js
+++ load_server.js
@@ -63,6 +63,7 @@ module.exports = () => {
           stateMachinePort: null,
           inventoryPort: null,
           viewerPort: null,
+          events: [],
           config: {
             job: 'none',
             mode: 'none',
